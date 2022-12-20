0ae8cf2f5e798e1cfb9a615777672484ebbe94c5
Anastasia <cristeaanastasia13@gmail.com>
2022-12-20 12:07:05 +0100

    Fixed problemo

---
+++ Assets/Scripts/GPS.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPS : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    /*IEnumerator GPSLoc()
+    {
+        //if()
+    }*/
+    void Update()
+    {
+        
+    }
+}
